20c3540122cbdb2ab07577462a409d7ed8d023ff
Matvei <sasinovichm@gmail.com>
2022-11-09 12:40:23 +0200

    seocnd commit

--- source/js/index.js
+++ source/js/index.js
@@ -3,5 +3,53 @@ let cards = document.querySelector('.cards');
 fetch('https://rickandmortyapi.com/api/character').then((response) => {
     return response.json()
 }).then((data) => {
-    console.log(data)
+    renderCards(data)
 })
+
+let search = (hero) => {
+    fetch(`https://rickandmortyapi.com/api/character?name=${hero}`).then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+    })}
+
+let renderCards = (heroes) => {
+    console.log(heroes)
+    heroes.results.forEach(item => {
+        cards.innerHTML += `
+        <div class="card">
+            <img src='${item.image}'></img>
+            <div class="card__name">${item.name}</div>
+            <div class="card__origin">${item.origin.name}</div>
+            <button class="card__btn" onclick="renderPopup(${item.id})">More</button>
+        </div>
+        `
+    });
+}
+
+let renderPopup = (id) => {
+    fetch(`https://rickandmortyapi.com/api/character/${id}`).then((response) => {
+        return response.json()
+    }).then((data) => {
+        document.body.innerHTML += `
+            <div class="popup">
+                <div class="popup__body">
+                    <div class="popup__content">
+                        <img src="${data.image}"></img>
+                        <button class="popup__close" onclick="popupRemove()">X</button>
+                        <table>
+                            <tr><td>Status:</td> <td>${data.status}</td></tr>
+                            <tr><td>Gender:</td> <td>${data.gender}</td></tr>
+                            <tr><td>Species:</td> <td>${data.species}</td></tr>
+                            <tr><td>Location:</td> <td>${data.location.name}</td></tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            `
+    })
+}
+
+let popupRemove = () => {
+    document.querySelector('.popup').remove();
+}
